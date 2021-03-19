0b39ad1af687cfec2caa267b017da4da9ea2a78e
Frederik <60892381+frederikme@users.noreply.github.com>
2021-03-19 09:53:34 +0100

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -1,4 +1,4 @@
 from sandboxed import is_sandboxed
 
-# chance will be between 0 and 1, whereas 0 is a real machine and 1 a virtual machine
+# certainty will be expressed with a value between 0 and 1, whereas closer to 0 is a real machine and closer to 1 a virtual machine
 chance = is_sandboxed(logging=True)
